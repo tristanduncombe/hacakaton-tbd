658cad5a6de60c8f9e17059bb4b4bb11325ecb3a
JaeWon Yun <alexyun0429@gmail.com>
2023-08-25 22:11:33 +1000

    merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "@mui/material";
 import Home from "./components/home/home";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Apple Color Emoji", sans-serif`,
+  },
+});
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <Home />
-            </header>
-        </div>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Home />
+        </header>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,5 +1,65 @@
 import React from "react";
+import { Grid, Typography } from "@mui/material";
+import CustomButton from "../button/Button";
+import ProjectCard from "../project_card/ProjectCard";
+
+import "./Home.css";
+
+type ProjectData = {
+  name: string;
+  updatedDate: string;
+  completion: string;
+};
+
+type HomeProps = {
+  data: ProjectData[];
+};
 
 export default function Home() {
-    return <div> Who's Horsing Around </div>;
+  // export default function Home({ data }: HomeProps) {
+  return (
+    <>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        className="home_title"
+      >
+        <Grid item>
+          <Typography variant="h5">Projects:</Typography>
+        </Grid>
+        <Grid item>
+          <CustomButton text="Add Project" />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} className="home_grid">
+        <Grid item xs={12}>
+          <Typography variant="body1" align="center">
+            No projects found
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+          <ProjectCard />
+        </Grid>
+      </Grid>
+
+      {/* <Grid container spacing={2} className="home_grid">
+        {data.length === 0 ? (
+          <Grid item xs={12}>
+            <Typography variant="body1" align="center">
+              No projects found
+            </Typography>
+          </Grid>
+        ) : (
+          data.map((project, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <ProjectCard data={project} />
+            </Grid>
+          ))
+        )}
+      </Grid> */}
+    </>
+  );
 }
