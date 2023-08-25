3cbbc4a2db461d26f85861410dcc0b786dc28364
Luke Graham <grahamluke128@gmail.com>
2023-08-25 23:06:12 +1000

    create create-project page

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,134 +1,84 @@
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import React, {useState} from "react";
-import { Fab, TextField } from '@mui/material';
-import NavigationIcon from '@mui/icons-material/Navigation';
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import CustomButton from "../button/Button";
+import ProjectCard from "../project_card/ProjectCard";
 
+import "./Home.css";
 
+type ProjectData = {
+  name: string;
+  updatedDate: string;
+  completion: string;
+};
 
-// const Item = styled(Paper)(({ theme }) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//     ...theme.typography.body2,
-//     padding: theme.spacing(1),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-//   }));
-
-
-
+type HomeProps = {
+  data: ProjectData[];
+};
 
 export default function Home() {
-    const [date, setDate] = useState()
+  // export default function Home({ data }: HomeProps) {
+  return (
+    <>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        className="home_title"
+      >
+        <Grid item>
+          <Typography variant="h5">Projects:</Typography>
+        </Grid>
+        <Grid item>
+          <CustomButton text="Add Project" />
+        </Grid>
+      </Grid>
 
-    const [rows, setRows] = useState([
-        { rating: '', name: ''},
-      ]);
-    
-      const addRow = () => {
-        setRows([...rows, { rating: '', name: ''}]);
-      };
-    
-      const handleChange = (event: any, index: any) => {
-        const { name, value } = event.target;
-        const newRows = [...rows];
-        newRows[index] = { ...newRows[index], [name]: value };
-        setRows(newRows);
-      };
+      <Grid container spacing={2} className="home_grid">
+        <Grid item xs={12}>
+          <Typography variant="body1" align="center">
+            No projects found
+          </Typography>
+        </Grid>
 
-      const handleDelete = (index: any) => {
-        const newRows = [...rows];
-        newRows.splice(index, 1);
-        setRows(newRows);
-      };
-      
-    function setValue(newValue: any): void {
-        throw new Error('Function not implemented.');
-    }
-    console.log(rows)
-    return (
-        <React.Fragment>
-            <div> Australian Polo X Association </div>
-            <h1> New Project </h1>
-            <Box sx={{width: '80%'}}>
-                <Grid container rowSpacing={1} sx={{backgroundColor: 'white'}} >
+        <Grid item xs={12} sm={6} md={4}>
+          <ProjectCard />
+        </Grid>
 
-                    <Grid item xs={6} sx={{backgroundColor: 'white'}}>
-                        <p>Competition Name</p>
-                    </Grid>
-                    <Grid item xs={6} sx={{backgroundColor: 'white'}}>
-                        <TextField id="outlined-basic" helperText="Please enter your name" variant="outlined" color="secondary" focused />
-                    </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ProjectCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ProjectCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ProjectCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ProjectCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ProjectCard />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <ProjectCard />
+        </Grid>
+      </Grid>
 
-                    <Grid item xs={6} sx={{backgroundColor: 'white'}}>
-                        <p>Date of Event</p>
-                    </Grid>
-                    <Grid item xs={6} sx={{backgroundColor: 'white'}}>
-                        <input type='date' onChange={(e:any)=>setDate(e.target.value)} />
-                    </Grid>
-
-
-                    <Grid item xs={6} sx={{backgroundColor: 'white'}}>
-                        <p>Field Information</p>
-                    </Grid>
-                    <Grid item xs={6} sx={{backgroundColor: 'white'}}>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Field Rating</th>
-                                    <th>Field Name</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {rows.map((row, index) => (
-                                    <tr key={index}>
-                                        <td>
-                                            <input
-                                            type="text"
-                                            name="rating"
-                                            value={row.rating}
-                                            onChange={(event) => handleChange(event, index)}
-                                            />
-                                        </td>
-                                        <td>
-                                            <input
-                                            type="text"
-                                            name="name"
-                                            value={row.name}
-                                            onChange={(event) => handleChange(event, index)}
-                                            />
-                                        </td>
-                                        <td>
-                                            <button onClick={() => handleDelete(index)}>Delete</button>
-                                        </td>
-                                
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-
-                        <button onClick={addRow}>Add Row</button>
-                        
-
-                    </Grid>
-                </Grid>
-            </Box>
-
-            <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <Fab variant="extended" size="small" color="primary">
-                    <NavigationIcon sx={{ mr: 1 }} />
-                    Continue
-                </Fab>
-            </Box>
-        </React.Fragment>
-    )
+      {/* <Grid container spacing={2} className="home_grid">
+        {data.length === 0 ? (
+          <Grid item xs={12}>
+            <Typography variant="body1" align="center">
+              No projects found
+            </Typography>
+          </Grid>
+        ) : (
+          data.map((project, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <ProjectCard data={project} />
+            </Grid>
+          ))
+        )}
+      </Grid> */}
+    </>
+  );
 }
-function useReactTable(arg0: any, addRow: any, arg2: () => void) {
-    throw new Error('Function not implemented.');
-}
-
-function getCoreRowModel() {
-    throw new Error('Function not implemented.');
-}
-
