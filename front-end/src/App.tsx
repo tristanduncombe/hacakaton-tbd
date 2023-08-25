be5a43ffe8792a09432695a5074bbf72eeb5064f
Tristan Duncombe <tduncombe747@gmail.com>
2023-08-25 18:23:38 +1000

    Fix Frontend

---
+++ front-end/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>CODE GOES HERE</div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
