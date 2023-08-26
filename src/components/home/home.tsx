398e88f36e1c06bf358f7faef45319c9beb34a00
Tay <tayla.r.ward@outlook.com>
2023-08-26 12:19:47 +1000

    Commented out my function calls

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { gradeGen } from "../common/dataGen";
 
 export default function Home() {
-    gradeGen()
+    //gradeGen()
     return <div> Who's Horsing Around </div>;
 }
