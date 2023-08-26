d534f88b5fde7ca6c417db0fb2e3b6153a0ca37f
Luke Graham <grahamluke128@gmail.com>
2023-08-26 13:01:39 +1000

    Added csv to json conversion

--- src/components/import/import.tsx
+++ src/components/import/import.tsx
@@ -1,22 +1,39 @@
 import React, { useRef } from "react";
 import CustomButton from "../button/Button";
 import { Button } from "@mui/material";
+import PapaParse from "papaparse";
 
 export default function Import() {
-
-
     const inputRef = useRef<HTMLInputElement>(null);
-
     const handleFileSelect = (event: any) => {
-    const file = event.target.files[0];
-    console.log(file);
+        const file = event.target.files[0];
+        console.log(file);
+        PapaParse.parse(file, {
+            header: true,
+            complete: (results: any) => {
+            console.log(results.data); // This is the JSON object
+
+            const fileData = JSON.stringify(results.data); 
+            const blob = new Blob([fileData], { type: "text/plain" }); 
+            const url = URL.createObjectURL(blob); 
+            const link = document.createElement("a"); 
+            link.download = "imported_data.json"; 
+            link.href = url; link.click();
+
+            /** Look at what kind of file it is:
+             * If its csv, do above...
+             * If its (excel? ) do this...
+             * Else say try again
+             */
+
+            },
+        });
     };
 
     
 
   return (
     <div className="App">
-
         <CustomButton text="Import CSV File" onClick={() => {
           if (inputRef.current) {
             // Call the click() method on inputRef.current
@@ -25,7 +42,5 @@ export default function Import() {
         }} /> 
       <input type="file" ref={inputRef} onChange={handleFileSelect} style={{ display: "none" }} />
     </div>
-    
   );
-
 }
