eba88de86e6902790585c52ce66a9a367e1e11ea
Tristan Duncombe <tduncombe747@gmail.com>
2023-08-26 17:13:00 +1000

    Add file system functions

--- src/renderer/components/Common/common.ts
+++ src/renderer/components/Common/common.ts
@@ -1,3 +1,24 @@
-export function common() {
-    return null;
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+export function getFiles(path: string) {
+  return fs.readdirSync(path);
+}
+
+export function getFilesByType(path: string, type: string) {
+  return fs.readdirSync(path).filter((item) => item.endsWith(type));
+}
+
+export function saveFile(path: string, data: string) {
+  fs.writeFileSync(path, data);
+}
+
+export function loadFile(path: string): string {
+  return fs.readFileSync(path, "utf-8");
+}
+
+export function getDocumentsPath(): string {
+  const homeDir = os.homedir();
+  return path.join(homeDir, "Documents");
 }
