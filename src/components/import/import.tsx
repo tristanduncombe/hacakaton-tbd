ff35be7f348a27f7437395dcfb673c362fbf5936
Luke Graham <grahamluke128@gmail.com>
2023-08-26 16:18:49 +1000

    fixing output of json files

--- src/components/import/import.tsx
+++ src/components/import/import.tsx
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import CustomButton from "../button/Button";
-import { Alert, AlertTitle, Button } from "@mui/material";
-import PapaParse from "papaparse";
+import { Alert, AlertTitle } from "@mui/material";
 import { read, utils} from "xlsx";
 import { useState } from "react";
 
@@ -11,7 +10,6 @@ export default function Import() {
 
 
     const handleFileSelect = (event: any) => {
-        const result = "error";
         const file = event.target.files[0];
         
         const fileName = file.name
@@ -19,42 +17,33 @@ export default function Import() {
         let validExtensions = ["xls", "xlsx", "xlsm"]; 
 
         if (validExtensions.includes(fileExtension)) { // If the file is an excel file
-            console.log("Got through the first one")
             const reader = new FileReader(); 
             reader.onload = (e: any) => { 
                 const data = e.target.result; 
                 const workbook = read(data, { type: "array" }); 
-                const sheetName = workbook.SheetNames[0]; 
-                const worksheet = workbook.Sheets[sheetName]; 
-                const json = utils.sheet_to_json(worksheet); 
-                console.log(json); // This is the JSON object
-                const fileData = JSON.stringify(json); 
-                const blob = new Blob([fileData], { type: "text/plain" }); 
-                const url = URL.createObjectURL(blob); 
-                const link = document.createElement("a"); 
-                link.download = "imported_data.json"; 
-                link.href = url; link.click(); 
+                let excelPages = []
+                const teamSheet = workbook.SheetNames[0]; 
+                const playerSheet = workbook.SheetNames[1]; 
+                const umpireSheet = workbook.SheetNames[4]; 
+                excelPages.push(teamSheet, playerSheet, umpireSheet);
+                let jsonObjects: {[page: string]: any} = {};
+                
+                excelPages.forEach(page => {
+                    const worksheet = workbook.Sheets[page]; 
+                    const json = utils.sheet_to_json(worksheet); 
+                    const fileData = JSON.stringify(json); 
+                    jsonObjects[page] = fileData;
+
+                    // const blob = new Blob([fileData], { type: "text/plain" }); 
+                    // const url = URL.createObjectURL(blob); 
+                    // const link = document.createElement("a"); 
+                    // link.download = `${page}.json`; 
+                    // link.href = url; link.click(); 
+
+                });
+                
             }; 
             reader.readAsArrayBuffer(file);
-            setResult('success');
-             
-
-        } else if (file.type == "text/csv") { // If the data is a csv file
-            console.log(fileExtension)
-            console.log(file.type)
-            console.log(file.name)
-            console.log("Got through the second one")
-            PapaParse.parse(file, {
-                header: true,
-                complete: (results: any) => {
-                console.log(results.data); // This is the JSON object
-                const fileData = JSON.stringify(results.data); 
-                const blob = new Blob([fileData], { type: "text/plain" }); 
-                const url = URL.createObjectURL(blob); 
-                const link = document.createElement("a"); 
-                link.download = "imported_data.json"; 
-                link.href = url; link.click();
-            }})
             setResult('success');
         } else {
             setResult('error');
@@ -80,14 +69,14 @@ export default function Import() {
           
           {result === "error" && (
             <Alert severity="error">
-              <AlertTitle>Error</AlertTitle> This is an error alert —{" "}
-              <strong>check it out!</strong>
+              <AlertTitle>Error</AlertTitle> You must select an excel document —{" "}
+              <strong>try again!</strong>
             </Alert>
           )}
           {result === "success" && (
             <Alert severity="success">
               <AlertTitle>Success</AlertTitle>
-              This is a success alert — <strong>check it out!</strong>
+              Successfully read given excel file — <strong>continue!</strong>
             </Alert>
           )}
         </div>
