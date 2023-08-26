b133c9b8ac0a3537217cf898682a736f36061bb2
Tay <tayla.r.ward@outlook.com>
2023-08-26 13:08:57 +1000

    Added dataGen file lol soz

---
+++ src/components/common/dataGen.ts
@@ -0,0 +1,130 @@
+import { any } from 'prop-types';
+import teamsJson from '../common/teams.json'
+
+
+/*
+This function spilts a competition into grades
+
+Requirements:
+The file passed needs to only contain the teams of one competition category and type.
+The file must already have the teams orders via points
+*/
+export function gradeGen() {
+    let teams = teamsJson;
+
+    // Check for invalid ammount of teams. I.e 0 or 1 teams
+    if (teams['teams'].length <= 1) {
+        return {
+            "status": "0",
+            "reason": "There is not enough teams in this category/type\nCategory: " + teams['Team Category'] +
+                "\nType: " + teams['Team Type']
+        };
+    }
+
+    // Need to group the teams together based on the points
+    // Requirements - have at least 3 teams in a grade - ideallaly 4 teams
+    // Group the closest points together
+    let gradings = groupTeams(teams);
+    
+    // Now need to even the grades out - the last grade could have 1 or 2 teams in it.
+    //gradings = evenGradesOut(gradings);
+    
+
+    return null;
+}
+
+
+/*
+This function is aiming to get the teams into groups of 4. 
+Only going up to 5 or down to 3 if needed
+
+Returns: An array of JSONS in the format of
+returnedTeams = {
+    "Team Category": "Category",
+    "Team Type": "Type",
+    "grades":
+    [
+        {
+            "grade": "A",
+            "teams": [
+                {}
+            ]
+        }
+    ]
+}
+*/
+function groupTeams(teams: any) {
+    let gradeLetter = 'A';
+    let returnTeams = {
+        "Team Category": teams['Team Category'],
+        "Team Type": teams['Team Type'],
+        "grades":
+            [] as Array<any>
+    }
+
+    const chunkSize = 4;
+    for (let i = 0; i < teams['teams'].length; i += chunkSize) {
+        let grade = {"grade": gradeLetter,
+            "teams": [] as Array<any>
+        };
+        var chunk = teams['teams'].slice(i, i + chunkSize);
+        grade['teams'] = grade['teams'].concat(chunk);
+
+        returnTeams['grades'].push(grade);
+        gradeLetter = nextChar(gradeLetter);
+    }
+
+    return returnTeams;
+
+}
+
+/*
+This function evens out the grades. As they are chunked together, 
+so the last grade could have 1 or 2 teams in it
+
+Returns:
+The same 'grades' var as given, but even out.
+*/
+/* function evenGradesOut(grades: any) {
+    let returnGrades = grades;
+
+    // If the last grade has two teams, need to merge up
+    if (grades['grades'][-1]['grades'].length == 2) {
+        // First situation, there are at least three grades
+        // Move a team from the second grade up one
+        // Concat bottom teams to the second grade
+        if (grades['grades'].length >= 3) {
+            // First move the first team from the second last grade up to the end of the next grade
+            grades['grades'][-3]['teams'].push(grades['grades'][-2]['teams'][0]);
+            grades['grades'][-2]['teams'].shift();
+
+            // Next, concat last grade to second grade. Then remove last grade
+            grades['grades'][-2]['teams'].concat(grades['grades'][-1]['teams'])
+            grades['grades'].pop();
+        }
+
+        // Second situation
+        // Only two grades, need to combine the two grades into one
+        else if () {
+
+        }
+        
+    } 
+    // If the grade has one team - need to combine with the grade above
+    else if (grades['teams'][-1]['teams'].length == 1) {
+        grades[-2]['teams'] = grades[-2]['teams'].concat(grades[-1]['teams']);
+        grades.pop();
+    }
+
+    return grades;
+} */
+
+function nextChar(c: any) {
+    if (c == 'Z') {
+        return '';
+    }
+    return String.fromCharCode(c.charCodeAt(0) + 1);
+}
+
+
+
